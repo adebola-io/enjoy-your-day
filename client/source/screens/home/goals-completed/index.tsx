74af05203028cb8939d264914bd0acdac9453e0a
adebola-io <adebolaakomolafe@gmail.com>
2024-12-22 19:28:02 +0100

    fix: show confetti only once

--- client/source/screens/home/goals-completed/index.tsx
+++ client/source/screens/home/goals-completed/index.tsx
@@ -6,11 +6,14 @@ import { Cell } from '@adbl/cells';
 import confetti from 'canvas-confetti';
 import classes from './goals-completed.module.css';
 
+let confettiDone = false;
 export function GoalsCompleted() {
   const observer = useObserver();
   const containerRef = Cell.source<HTMLDivElement | null>(null);
 
   observer.onConnected(containerRef, () => {
+    if (confettiDone) return;
+    confettiDone = true;
     confetti({
       particleCount: 300,
       gravity: 0.8,
@@ -24,9 +27,10 @@ export function GoalsCompleted() {
   return (
     <div ref={containerRef} class={classes.container} data-stagger-children>
       <ConfettiIcon class={classes.icon} />
-      <h1>Congrats to you!</h1>
-      <p>
-        You completed all your goals. Well done! I hope you enjoyed your day.
+      <h1 class={classes.heading}>Congrats to you!</h1>
+      <p class={classes.text}>
+        Well done! You completed all your goals. I hope you had an interesting
+        day.
       </p>
       <Button rounded variant="secondary">
         Plan for tomorrow

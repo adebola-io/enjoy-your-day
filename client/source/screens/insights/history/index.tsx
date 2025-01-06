da662dba4fae6a52b8f73ae2b7507981e1c6d822
adebola-io <adebolaakomolafe@gmail.com>
2025-01-06 04:35:42 +0100

    fix: remove unused props parameter in History component

--- client/source/screens/insights/history/index.tsx
+++ client/source/screens/insights/history/index.tsx
@@ -3,5 +3,6 @@ import classes from './history.module.css';
 export interface InsightHistoryProps {}
 
 export function History(props: InsightHistoryProps) {
+  props;
   return <div class={classes.container}>History</div>;
 }

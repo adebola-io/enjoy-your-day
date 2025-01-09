0f6b521b599ac7733d1a0296e4ec6ded79813538
adebola-io <adebolaakomolafe@gmail.com>
2025-01-09 18:09:34 +0100

    feat: add more icons

--- client/source/components/icons/angle-brackets.tsx
+++ client/source/components/icons/angle-brackets.tsx
@@ -2,10 +2,20 @@ import type { IconProps } from './props';
 
 export function AngleBracketsIcon(props: IconProps) {
   return (
-    <svg viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      
-<path d="M14.5833 16.6667L6.25 24.359L14.5833 33.3334M35.4167 16.6667L43.75 24.359L35.4167 33.3334M29.1667 8.33337L20.8333 41.6667" stroke="currentColor" stroke-width="4.16667" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg"/>>
-
+    <svg
+      viewBox="0 0 50 50"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M14.5833 16.6667L6.25 24.359L14.5833 33.3334M35.4167 16.6667L43.75 24.359L35.4167 33.3334M29.1667 8.33337L20.8333 41.6667"
+        stroke="currentColor"
+        stroke-width="4.16667"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        xmlns="http://www.w3.org/2000/svg"
+      />
     </svg>
   );
 }

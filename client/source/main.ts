4f9a48b33ba3c0ccd119d62059500ee99c727290
Sefunmi <adebolaakomolafe@gmail.com>
2025-01-11 07:36:02 +0100

    fix: reduce wait time

--- client/source/main.ts
+++ client/source/main.ts
@@ -47,7 +47,7 @@ export async function resumeApp() {
   // sometimes waits too long and bleeds into the start of the
   // waiting screen animation. The delay is meant to prevent the
   // app from loading until the splash screen is gone.
-  await new Promise((resolve) => setTimeout(resolve, 400));
+  await new Promise((resolve) => setTimeout(resolve, 200));
 
   const waitingScreen = document.querySelector('#waiting-screen');
   waitingScreen?.classList.add('loading');

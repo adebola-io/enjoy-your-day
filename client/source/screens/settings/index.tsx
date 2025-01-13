f2b0d3412776c883cc8e1ccc2f1b55ffa395fd73
adebola-io <adebolaakomolafe@gmail.com>
2025-01-13 06:46:07 +0100

    fix: remove unused spin animation function from settings component

--- client/source/screens/settings/index.tsx
+++ client/source/screens/settings/index.tsx
@@ -1,19 +1,6 @@
 import { Loader } from '#/components/loader';
 
 export default function Settings() {
-  const spin = function (this: HTMLElement) {
-    this.animate(
-      [
-        {
-          transform: 'rotate3d(0, 1, 0, -78deg) translate(80%, 50%)',
-        },
-        {
-          transform: 'rotate3d(0, 1, 0, 0deg)',
-        },
-      ],
-      { duration: 900, easing: 'cubic-bezier(0.4, 0, 0.2, 1)' }
-    );
-  };
   return (
     <div
       style={{

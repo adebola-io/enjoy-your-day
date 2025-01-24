8fc7093b8df0c55fd229ef231df23b0cc4be71bb
adebola-io <adebolaakomolafe@gmail.com>
2025-01-24 16:35:36 +0100

    refactor: simplify server startup logic and remove unused dependencies

--- server/src/main.rs
+++ server/src/main.rs
@@ -16,35 +16,29 @@ use std::{net::SocketAddr, sync::Arc};
 
 #[tokio::main]
 async fn main() {
-    match dotenv() {
-        Ok(_) => {
-            Server::bind(
-                &std::env::var("PORT")
-                    .map(|port| format!("0.0.0.0:{}", port))
-                    .unwrap_or_else(|_| "0.0.0.0:7860".to_string())
-                    .parse::<SocketAddr>()
-                    .expect("Could not parse SocketAddr."),
-            )
-            .serve(
-                Router::new()
-                    .route("/", get(routes::get_index))
-                    .route("/notify", post(routes::post_send_notification_to_user))
-                    .route("/echo", post(routes::post_echo))
-                    .route("/register", post(routes::post_register_user))
-                    .route("/delete", delete(routes::delete_user))
-                    .route("/users", get(routes::get_users))
-                    .layer(Extension(Arc::new(
-                        AppState::new(get_service_account_path(), get_admin_password())
-                            .await
-                            .start_checkpoint_loop(),
-                    )))
-                    .into_make_service(),
-            )
-            .await
-            .expect("Server could not be started.");
-        }
-        Err(e) => {
-            panic!("Could not read env variables from environment: {:?}", e)
-        }
-    }
+    dotenv().ok();
+    Server::bind(
+        &std::env::var("PORT")
+            .map(|port| format!("0.0.0.0:{}", port))
+            .unwrap_or_else(|_| "0.0.0.0:7860".to_string())
+            .parse::<SocketAddr>()
+            .expect("Could not parse SocketAddr."),
+    )
+    .serve(
+        Router::new()
+            .route("/", get(routes::get_index))
+            .route("/notify", post(routes::post_send_notification_to_user))
+            .route("/echo", post(routes::post_echo))
+            .route("/register", post(routes::post_register_user))
+            .route("/delete", delete(routes::delete_user))
+            .route("/users", get(routes::get_users))
+            .layer(Extension(Arc::new(
+                AppState::new(get_service_account_path(), get_admin_password())
+                    .await
+                    .start_checkpoint_loop(),
+            )))
+            .into_make_service(),
+    )
+    .await
+    .expect("Server could not be started.");
 }

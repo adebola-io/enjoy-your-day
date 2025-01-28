d7230efb8cd8ceef05b093eae848376b3f6b478b
adebola-io <adebolaakomolafe@gmail.com>
2025-01-28 07:25:36 +0100

    refactor: remove unused dailyGoals state and simplify appIsLoaded logic

--- client/source/screens/index.tsx
+++ client/source/screens/index.tsx
@@ -1,6 +1,6 @@
 import { NavigationBar } from '#/components/navigation-bar';
 import { ViewLayer, ViewLayerGroup } from '#/components/view-layer';
-import { appLoadingState, dailyGoals } from '#/data/state';
+import { appLoadingState } from '#/data/state';
 import { Cell } from '@adbl/cells';
 import { useRouter } from '@adbl/unfinished/router';
 import { If } from '@adbl/unfinished';
@@ -10,23 +10,16 @@ import classes from './app.module.css';
 
 export default function App() {
   const { Outlet } = useRouter();
-
-  const appIsLoaded = Cell.derived(() => {
-    return appLoadingState.value === 'done';
-  });
+  const appIsLoaded = Cell.derived(() => appLoadingState.value === 'done');
 
   appIsLoaded.runAndListen((appIsLoaded) => {
     if (appIsLoaded) registerNotificationServiceWorker();
   });
 
-  const dailyGoalsLoaded = Cell.derived(() => {
-    return dailyGoals.value.length > 0;
-  });
-
   return (
     <ViewLayerGroup>
       <ViewLayer class={classes.appContent} open data-app-loaded={appIsLoaded}>
-        <Outlet class={classes.outlet} data-goals-loaded={dailyGoalsLoaded} />
+        <Outlet class={classes.outlet} />
         {If(appIsLoaded, NavigationBar)}
       </ViewLayer>
       <AutoSelectLayer />

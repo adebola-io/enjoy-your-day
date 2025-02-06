507a2c9ace1d74d33ce01c58a8e37391d749f19f
adebola-io <adebolaakomolafe@gmail.com>
2025-02-06 13:19:22 +0100

    feat: add WristWatch, Superbowl, Grammy, and ChargingBattery icons

---
+++ client/source/components/icons/wrist-watch.tsx
@@ -0,0 +1,41 @@
+import type { IconProps } from './props';
+
+export function WristWatchIcon(props: IconProps) {
+  return (
+    <svg
+      viewBox="0 0 50 51"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M35.4167 13.7083L34.7836 10.8588C34.0884 7.73044 33.7407 6.16623 32.5982 5.24975C31.4557 4.33325 29.8534 4.33325 26.6486 4.33325H23.3515C20.1468 4.33325 18.5444 4.33325 17.4019 5.24975C16.2594 6.16623 15.9118 7.73044 15.2166 10.8588L14.5834 13.7083"
+        stroke="currentColor"
+        stroke-width="3.125"
+        xmlns="http://www.w3.org/2000/svg"
+      />
+      <path
+        d="M35.4167 36.625L34.7836 39.4744C34.0884 42.6029 33.7407 44.1671 32.5982 45.0835C31.4557 46 29.8534 46 26.6486 46H23.3515C20.1468 46 18.5444 46 17.4019 45.0835C16.2594 44.1671 15.9118 42.6029 15.2166 39.4744L14.5834 36.625"
+        stroke="currentColor"
+        stroke-width="3.125"
+        xmlns="http://www.w3.org/2000/svg"
+      />
+      <path
+        d="M40.625 25.1666C40.625 33.796 33.6294 40.7916 25 40.7916C16.3705 40.7916 9.375 33.796 9.375 25.1666C9.375 16.5372 16.3705 9.54163 25 9.54163C33.6294 9.54163 40.625 16.5372 40.625 25.1666Z"
+        stroke="currentColor"
+        stroke-width="3.125"
+        xmlns="http://www.w3.org/2000/svg"
+      />
+      <path
+        d="M25 18.7559V25.1662L29.1667 29.1725"
+        stroke="currentColor"
+        stroke-width="3.125"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        xmlns="http://www.w3.org/2000/svg"
+      />
+    </svg>
+  );
+}
+
+export default WristWatchIcon;

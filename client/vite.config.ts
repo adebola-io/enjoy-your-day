d70e4003338be97f2883767adf53bf83460747c3
adebola-io <adebolaakomolafe@gmail.com>
2025-02-06 18:04:41 +0100

    fix: disable PWA auto-update during development

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
-        enabled: true,
+        enabled: false,
       },
       manifest: {
         name: 'Enjoy Your Day',

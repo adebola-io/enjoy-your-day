179b55d4e5dfda87ecf3ad961858eea490787eee
adebola-io <adebolaakomolafe@gmail.com>
2025-02-07 17:00:36 +0100

    fix: drawer pending state scale

--- client/source/screens/onboarding/loading/index.tsx
+++ client/source/screens/onboarding/loading/index.tsx
@@ -4,8 +4,8 @@ import { createUser } from '#/services/database';
 import { ONBOARDING_LOADING_DELAY } from '#/data/constants';
 import { useRouter } from '@adbl/unfinished/router';
 import { setMetaTheme } from '#/library/utils';
-import classes from './loading.module.css';
 import { CSS_VARS } from '#/styles/variables';
+import classes from './loading.module.css';
 
 export default function Loading() {
   const router = useRouter();
@@ -22,28 +22,20 @@ export default function Loading() {
 
   const handleAnimationEnd = async (event: AnimationEvent) => {
     if (!event.animationName.includes('fade-to-white')) return;
-    await Promise.all([
-      createUser(username.value),
-      new Promise<void>(onboardingPromiseCallback),
-    ]);
+    await createUser(username.value);
+    await new Promise((r) => setTimeout(r, ONBOARDING_LOADING_DELAY));
+    appLoadingState.value = 'done';
     router.useViewTransitions = false;
     router.replace('/home');
   };
 
   return (
     <div
+      class={classes.onboardingViewFinalLoaderContainer}
       onAnimationStart--self={handleAnimationStart}
       onAnimationEnd--self={handleAnimationEnd}
-      class={classes.onboardingViewFinalLoaderContainer}
     >
       <Loader class={classes.onboardingViewFinalLoader} />
     </div>
   );
-}
-
-function onboardingPromiseCallback(resolve: () => void) {
-  setTimeout(() => {
-    appLoadingState.value = 'done';
-    resolve();
-  }, ONBOARDING_LOADING_DELAY);
 }
